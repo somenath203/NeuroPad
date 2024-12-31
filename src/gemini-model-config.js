c587f206822a3de0a0e59f63c7cfab62383a4b7e
Somenath Choudhury <somenathchoudhury35@gmail.com>
2024-12-31 05:43:43 +0530

    commited

--- src/gemini-model-config.js
+++ src/gemini-model-config.js
@@ -2,8 +2,6 @@ import { google } from "@ai-sdk/google";
 import { streamText } from "ai";
 
 
-// in .env, the key needs to be named as "GOOGLE_GENERATIVE_AI_API_KEY" in order for this to work
-
 export const generate_text_by_google_gemini = async (messages) => {
 
   try {
